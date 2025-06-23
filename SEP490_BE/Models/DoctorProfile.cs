e17bc17744db77f65654308e686a98f7d2942cde
ManhToan16 <toanluon507@gmail.com>
2025-06-23 14:54:19 +0700

    create mapper and controller doctorProfile

--- SEP490_BE/Models/DoctorProfile.cs
+++ SEP490_BE/Models/DoctorProfile.cs
@@ -10,6 +10,7 @@ namespace SEP490_BE.Models
         public string? Qualifications { get; set; }
         public int? YearsOfExperience { get; set; }
         public string? Biography { get; set; }
+        public string? Avatar { get; set; }
 
         public virtual User Doctor { get; set; } = null!;
     }
