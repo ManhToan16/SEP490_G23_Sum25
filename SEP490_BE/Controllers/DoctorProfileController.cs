6faeada5cee5c0226ca75b364826a53feaf038c3
ManhToan16 <toanluon507@gmail.com>
2025-06-24 09:27:36 +0700

    Update DoctorProfileController.cs
    remove try-catch controller

--- SEP490_BE/Controllers/DoctorProfileController.cs
+++ SEP490_BE/Controllers/DoctorProfileController.cs
@@ -24,8 +24,7 @@ namespace SEP490_BE.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetDoctorProfile(string id)
         {
-            try
-            {
+          
                 var dto = await _doctorProfileService.GetById(id);
                 return Ok(new ApiResponse
                 {
@@ -34,60 +33,28 @@ namespace SEP490_BE.Controllers
                     Message = MessageConstants.GET_SUCCESS,
                     Data = dto
                 });
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                return NotFound(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status404NotFound,
-                    Success = false,
-                    Message = ex.Message,
-                    Data = null
-                });
-            }
+            
+           
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateDoctorProfile([FromBody] CreateDoctorProfileDTO dto)
         {
-            try
+
+            var createdDto = await _doctorProfileService.Create(dto);
+            return Ok(new ApiResponse
             {
-                var createdDto = await _doctorProfileService.Create(dto);
-                return Ok(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status201Created,
-                    Success = true,
-                    Message = MessageConstants.POST_SUCCESS,
-                    Data = createdDto
-                });
-            }
-            catch (ConflictDataException ex)
-            {
-                return Conflict(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status409Conflict,
-                    Success = false,
-                    Message = ex.Message,
-                    Data = null
-                });
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                return NotFound(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status404NotFound,
-                    Success = false,
-                    Message = ex.Message,
-                    Data = null
-                });
-            }
+                StatusCode = StatusCodes.Status201Created,
+                Success = true,
+                Message = MessageConstants.POST_SUCCESS,
+                Data = createdDto
+            });
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateDoctorProfile(string id, [FromBody] UpdateDoctorProfileDTO dto)
         {
-            try
-            {
+           
                 var updatedDto = await _doctorProfileService.Update(id, dto);
                 return Ok(new ApiResponse
                 {
@@ -96,24 +63,13 @@ namespace SEP490_BE.Controllers
                     Message = MessageConstants.PUT_SUCCESS,
                     Data = updatedDto
                 });
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                return NotFound(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status404NotFound,
-                    Success = false,
-                    Message = ex.Message,
-                    Data = null
-                });
-            }
+          
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteDoctorProfile(string id)
         {
-            try
-            {
+            
                 await _doctorProfileService.Delete(id);
                 return Ok(new ApiResponse
                 {
@@ -122,17 +78,7 @@ namespace SEP490_BE.Controllers
                     Message = MessageConstants.DELETE_SUCCESS,
                     Data = null
                 });
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                return NotFound(new ApiResponse
-                {
-                    StatusCode = StatusCodes.Status404NotFound,
-                    Success = false,
-                    Message = ex.Message,
-                    Data = null
-                });
-            }
+  
         }
 
         [HttpGet]
