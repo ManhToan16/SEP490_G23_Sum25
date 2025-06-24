ec536b03a2337921ac7d0ee16fa548da4f39c793
Trần Minh Quang <quangtmhe171602@fpt.edu.vn>
2025-06-24 23:18:50 +0700

    Update Auth Module: Email Service, Forgot Password function.

---
+++ SEP490_BE/DTO/AuthDTO/ForgotPasswordDTO.cs
@@ -0,0 +1,12 @@
+﻿using SEP490_BE.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEP490_BE.DTO.AuthDTO
+{
+    public class ForgotPasswordDTO
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = MessageConstants.INVALID_EMAIL)]
+        public string Email { get; set; }
+    }
+}
