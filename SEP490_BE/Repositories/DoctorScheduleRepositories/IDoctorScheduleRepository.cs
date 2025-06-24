ace3f5df36a127cb8bf79b68cd02b465bc0368d4
ManhToan16 <toanluon507@gmail.com>
2025-06-24 14:45:25 +0700

    create DoctorSchedule Service and Repo

---
+++ SEP490_BE/Repositories/DoctorScheduleRepositories/IDoctorScheduleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.DoctorScheduleRepositories
+{
+    public interface IDoctorScheduleRepository
+    {
+        Task<DoctorSchedule> FindByIdAsync(string id);
+        Task<DoctorSchedule> FindByDoctorIdAndDateAsync(string doctorId, DateTime date);
+        Task<(List<DoctorSchedule> Schedules, int TotalItems)> FindAll(
+            string? doctorId,
+            DateTime? date,
+            bool? isAvailable,
+            int pageNumber,
+            int pageSize);
+        Task InsertAsync(DoctorSchedule schedule);
+        Task UpdateAsync(DoctorSchedule schedule);
+        Task DeleteAsync(DoctorSchedule schedule);
+    }
+}
---
+++ SEP490_BE/Services/DoctorScheduleServices/IDoctorScheduleService.cs
@@ -0,0 +1,19 @@
+﻿using SEP490_BE.DTO.DoctorScheduleDTO;
+using SEP490_BE.DTO;
+
+namespace SEP490_BE.Services.DoctorScheduleServices
+{
+    public interface IDoctorScheduleService
+    {
+        Task<Pagination<DoctorScheduleResponseDTO>> GetAll(
+            string? doctorId,
+            DateTime? date,
+            bool? isAvailable,
+            int pageNumber,
+            int pageSize);
+        Task<DoctorScheduleResponseDTO> GetById(string id);
+        Task<DoctorScheduleResponseDTO> Create(CreateDoctorScheduleDTO request);
+        Task<DoctorScheduleResponseDTO> Update(string id, UpdateDoctorScheduleDTO request);
+        Task Delete(string id);
+    }
+}
