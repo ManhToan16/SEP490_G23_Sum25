fe1b6f102b7407364a740c330718ce574e598ea2
ManhToan16 <toanluon507@gmail.com>
2025-06-25 00:35:43 +0700

    create ExaminationRoom Modules: create Repo

---
+++ SEP490_BE/Repositories/ExaminationRoomRepositories/ExaminationRoomRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.ExaminationRoomRepositories
+{
+    public class ExaminationRoomRepository : IExaminationRoomRepository
+    {
+        private readonly KhanhAnNeurologyClinicContext _context;
+
+        public ExaminationRoomRepository(KhanhAnNeurologyClinicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ExaminationRoom> FindByIdAsync(string id)
+        {
+            return await _context.ExaminationRooms
+                .Include(er => er.DoctorSchedules)
+                .Include(er => er.Queues)
+                .FirstOrDefaultAsync(er => er.Id == id);
+        }
+
+        public async Task<(List<ExaminationRoom> Rooms, int TotalItems)> FindAll(
+            string? name,
+            string? description,
+            int pageNumber,
+            int pageSize)
+        {
+            var query = _context.ExaminationRooms.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where(er => er.Name.Contains(name));
+            }
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                query = query.Where(er => er.Description.Contains(description));
+            }
+
+            var totalItems = await query.CountAsync();
+            var rooms = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (rooms, totalItems);
+        }
+
+        public async Task InsertAsync(ExaminationRoom room)
+        {
+            await _context.ExaminationRooms.AddAsync(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ExaminationRoom room)
+        {
+            _context.ExaminationRooms.Update(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ExaminationRoom room)
+        {
+            _context.ExaminationRooms.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SEP490_BE/Repositories/ExaminationRoomRepositories/IExaminationRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.ExaminationRoomRepositories
+{
+    public interface IExaminationRoomRepository
+    {
+        Task<ExaminationRoom> FindByIdAsync(string id);
+        Task<(List<ExaminationRoom> Rooms, int TotalItems)> FindAll(
+            string? name,
+            string? description,
+            int pageNumber,
+            int pageSize);
+        Task InsertAsync(ExaminationRoom room);
+        Task UpdateAsync(ExaminationRoom room);
+        Task DeleteAsync(ExaminationRoom room);
+    }
+}
