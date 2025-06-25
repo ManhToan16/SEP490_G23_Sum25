10e8c0d2c050d2119e37c37bcf8625687dd13fea
ManhToan16 <toanluon507@gmail.com>
2025-06-25 22:04:14 +0700

    create repo for TechnicianSchedule

---
+++ SEP490_BE/Repositories/TechinicianScheduleRepositories/ITechnicianScheduleRepository.cs
@@ -0,0 +1,19 @@
+﻿using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.TechinicianScheduleRepositories
+{
+    public interface ITechnicianScheduleRepository
+    {
+        Task<TechnicianSchedule> FindByIdAsync(string id);
+        Task<TechnicianSchedule> FindByTechnicianIdAndDateAsync(string technicianId, DateTime date);
+        Task<(List<TechnicianSchedule> Schedules, int TotalItems)> FindAll(
+            string? technicianId,
+            DateTime? date,
+            int pageNumber,
+            int pageSize);
+        Task InsertAsync(TechnicianSchedule schedule);
+        Task UpdateAsync(TechnicianSchedule schedule);
+        Task DeleteAsync(TechnicianSchedule schedule);
+        Task<TechnicianSchedule> FindByRoomAndDateAsync(string laboratoryRoomId, DateTime date); // Kiểm tra trùng phòng và ngày
+    }
+}
