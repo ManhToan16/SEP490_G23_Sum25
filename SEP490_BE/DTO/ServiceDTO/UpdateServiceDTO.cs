f129067c9bb9c9bc732391e435612036d4fa3a35
ManhToan16 <toanluon507@gmail.com>
2025-06-27 11:22:02 +0700

    create DTO for Service modules

---
+++ SEP490_BE/DTO/ServiceDTO/UpdateServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SEP490_BE.DTO.ServiceDTO
+{
+    public class UpdateServiceDTO
+    {
+        public string? LaboratoryRoomId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
