4c84442462700b41f58a87b3ecc440fb1b8fd21f
ManhToan16 <toanluon507@gmail.com>
2025-06-27 11:34:19 +0700

    Update IServiceService.cs

--- SEP490_BE/Services/ServiceServices/IServiceService.cs
+++ SEP490_BE/Services/ServiceServices/IServiceService.cs
@@ -1,6 +1,21 @@
-﻿namespace SEP490_BE.Services.ServiceServices
+﻿using SEP490_BE.DTO.ServiceDTO;
+using SEP490_BE.DTO;
+
+namespace SEP490_BE.Services.ServiceServices
 {
-    public class IServiceService
+    public interface IServiceService
     {
+        Task<Pagination<ServiceResponseDTO>> GetAll(
+            string? laboratoryRoomId,
+            string? name,
+            decimal? minPrice,
+            decimal? maxPrice,
+            string? description,
+            int pageNumber,
+            int pageSize);
+        Task<ServiceResponseDTO> GetById(string id);
+        Task<ServiceResponseDTO> Create(CreateServiceDTO request);
+        Task<ServiceResponseDTO> Update(string id, UpdateServiceDTO request);
+        Task Delete(string id);
     }
 }
