602003634bcfd03aa1b4458b4c9f8136f344ccad
Vu Minh Quang <quangvmhe160498@fpt.edu.vn>
2025-07-11 00:29:54 +0700

    Update health check endpoints in CI/CD workflow and adjust Swagger usage in Program.cs

--- SEP490_BE/Program.cs
+++ SEP490_BE/Program.cs
@@ -152,11 +152,10 @@ builder.Services.AddScoped<IScheduleChangeRepository, ScheduleChangeRepository>(
 
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
+// dể luôn bật kể kả production
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 CreateAdmin(app.Services);
 
 app.UseMiddleware<UnsupportedMediaTypeMiddleware>();
