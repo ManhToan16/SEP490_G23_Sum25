ad2c7b8efb46e9560a191b5e47184f69df28f445
ManhToan16 <toanluon507@gmail.com>
2025-07-11 01:59:21 +0700

    create Repository for TimeSlot modules

---
+++ SEP490_BE/Repositories/TimeSlotRepositories/ITimeSlotRepository.cs
@@ -0,0 +1,10 @@
+﻿using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.TimeSlotRepositories
+{
+    public interface ITimeSlotRepository
+    {
+        Task<TimeSlot> FindByIdAsync(string id);
+        Task<List<TimeSlot>> GetAllAsync();
+    }
+}
