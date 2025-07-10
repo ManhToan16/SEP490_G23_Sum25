b344ca025568ea6adc4a10c4195ee2df5603a198
ManhToan16 <toanluon507@gmail.com>
2025-07-11 01:59:27 +0700

    create Services for TimeSlot modules

---
+++ SEP490_BE/Services/TimeSlotServices/ITimeSlotService.cs
@@ -0,0 +1,11 @@
+﻿using SEP490_BE.DTO.SupplierDTO;
+using SEP490_BE.DTO.TimeSlotDTO;
+
+namespace SEP490_BE.Services.TimeSlotServices
+{
+    public interface ITimeSlotService
+    {
+        Task<TimeSlotResponseDTO> GetTimeSlotById(string id);
+        Task<List<TimeSlotResponseDTO>> GetAllTimeSlots();
+    }
+}
