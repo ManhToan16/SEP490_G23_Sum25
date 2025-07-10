480831f9c9aea474d944d556f865108bd30b885b
ManhToan16 <toanluon507@gmail.com>
2025-07-11 01:24:05 +0700

    create Repository for Supplier modules

---
+++ SEP490_BE/Repositories/SupplierRepositories/ISupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using SEP490_BE.Entities;
+
+namespace SEP490_BE.Repositories.SupplierRepositories
+{
+    public interface ISupplierRepository
+    {
+        Task<Supplier> FindByIdAsync(string id);
+        Task AddAsync(Supplier supplier);
+        Task UpdateAsync(Supplier supplier);
+        Task DeleteAsync(string id);
+        Task<List<Supplier>> GetAllAsync();
+    }
+}
