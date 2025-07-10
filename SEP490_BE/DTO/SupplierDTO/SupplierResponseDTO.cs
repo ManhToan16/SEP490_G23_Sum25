171b13f03f9a720e3025341153e6e437b48b7c96
ManhToan16 <toanluon507@gmail.com>
2025-07-11 01:23:49 +0700

    create DTO for Supplier modules

---
+++ SEP490_BE/DTO/SupplierDTO/SupplierResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace SEP490_BE.DTO.SupplierDTO
+{
+    public class SupplierResponseDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
