73dffc54b3570ee9bff82e5a0b8c724407bf114c
ManhToan16 <toanluon507@gmail.com>
2025-07-14 18:55:03 +0700

    Add DTOs for material creation, update, and response
    Introduced CreateMaterialDTO, UpdateMaterialDTO, and MaterialResponseDTO classes to handle material data transfer for creation, updating, and response operations. These DTOs define the structure for material-related API requests and responses.

---
+++ SEP490_BE/DTO/MaterialDTO/MaterialResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace SEP490_BE.DTO.MaterialDTO
+{
+    public class MaterialResponseDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string SupplierId { get; set; }
+        public string? SupplierName { get; set; }
+        public string Unit { get; set; }
+        public int QuantityInStock { get; set; }
+        public int? MaxQuantity { get; set; }
+        public int? MinQuantity { get; set; }
+        public string CreatedAt { get; set; }
+        public string? UpdatedAt { get; set; }
+    }
+}
