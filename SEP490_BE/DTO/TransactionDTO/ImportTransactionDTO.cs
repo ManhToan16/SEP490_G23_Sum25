f6daa8cae6f732e4d12ad5eac8b1e4e4b75828d2
ManhToan16 <toanluon507@gmail.com>
2025-07-15 11:53:05 +0700

    Add DTOs for import and transaction responses
    Introduced ImportMaterialDTO and TransactionResponseDTO classes to represent import transaction data and transaction response data, respectively. These DTOs will facilitate data transfer for transaction-related operations.

---
+++ SEP490_BE/DTO/TransactionDTO/ImportTransactionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SEP490_BE.DTO.TransactionDTO
+{
+    public class ImportMaterialDTO
+    {
+        public string MaterialId { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int DefectiveQuantity { get; set; }
+        public DateTime ImportDate { get; set; }
+        public TimeSpan ImportTime { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string SupplierId { get; set; } = null!;
+        public string? Reason { get; set; }
+    }
+}
---
+++ SEP490_BE/DTO/TransactionDTO/TransactionResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace SEP490_BE.DTO.TransactionDTO
+{
+    public class TransactionResponseDTO
+    {
+        public string Id { get; set; }
+        public string MaterialId { get; set; }
+        public string? MaterialName { get; set; }
+        public string TransactionType { get; set; }
+        public int Quantity { get; set; }
+        public int? DefectiveQuantity { get; set; }
+        public string? RoomId { get; set; }
+        public string? RoomType { get; set; }
+        public string UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Reason { get; set; }
+        public string Status { get; set; }
+        public string  CreatedAt { get; set; }
+        public string? UpdatedAt { get; set; }
+        public decimal? Price { get; set; }
+        public string? SupplierId { get; set; }
+        public string? SupplierName { get; set; }
+    }
+}
