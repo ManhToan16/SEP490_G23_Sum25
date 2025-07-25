bf93d060f800a03364e3f7f9ce0b94240e7624e5
ManhToan16 <toanluon507@gmail.com>
2025-07-26 01:16:02 +0700

    create LabRoomTests NUnitTest

--- SEP490_BE/DTO/LaboratoryRoomDTO/UpdateLaboratoryRoomDTO.cs
+++ SEP490_BE/DTO/LaboratoryRoomDTO/UpdateLaboratoryRoomDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace SEP490_BE.DTO.LaboratoryRoomDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEP490_BE.DTO.LaboratoryRoomDTO
 {
     public class UpdateLaboratoryRoomDTO
     {
-        public string? Name { get; set; }
+        [Required(ErrorMessage = "Tên phòng là bắt buộc.")]
+        [MaxLength(100, ErrorMessage = "Tên phòng không được vượt quá 100 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sÀ-ỹ]+$", ErrorMessage = "Tên phòng không được chứa ký tự đặc biệt.")]
+        public string Name { get; set; }
+        [MaxLength(200, ErrorMessage = "Mô tả không được vượt quá 200 ký tự.")]
         public string? Description { get; set; }
     }
 }
