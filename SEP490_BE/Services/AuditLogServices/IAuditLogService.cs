9f70ac0a99eb37c6111219339fc5f74e36813b00
Trần Minh Quang <quangtmhe171602@fpt.edu.vn>
2025-08-06 22:57:24 +0700

    Update Audit Log

--- SEP490_BE/Services/AuditLogServices/IAuditLogService.cs
+++ SEP490_BE/Services/AuditLogServices/IAuditLogService.cs
@@ -1,11 +1,12 @@
 ﻿using SEP490_BE.DTO.UserDTO;
 using SEP490_BE.DTO;
 using SEP490_BE.Entities;
+using SEP490_BE.DTO.AuthDTO;
 
 namespace SEP490_BE.Services.AuditLogServices
 {
     public interface IAuditLogService
     {
-        Task<Pagination<AuditLog>> GetLogsAsync(string? userId, string? action, string? tableName, string? recordId, int pageNumber, int pageSize);
+        Task<Pagination<AuditLogDTO>> GetLogsAsync(string? userId, string? action, string? tableName, string? recordId, int pageNumber, int pageSize);
     }
 }
